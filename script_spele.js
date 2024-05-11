bbaf4014409333ac8d980661ec85e7c234a4b47e
Artemijs Skvorcevics <artskv07@gmail.com>
2024-05-11 11:29:28 +0300

    main

--- script_spele.js
+++ script_spele.js
@@ -24,23 +24,34 @@ The_Rocket.style.height = ((The_Element.clientHeight / 100) * 25)
 
 function Metres(that) {
     var fuel = parseFloat(that.parentNode.querySelector(".Game_rocket1").getAttribute("fuel")).toFixed(1)
-
+    document.body.querySelector("main .Game_Main .Game_Canvas_Borders .Game_Canvas").setAttribute("playing", true)
     if(fuel > 0) {
    fuel -= 0.1
    fuel = fuel.toFixed(1);
    that.parentNode.querySelector(".Game_rocket1").setAttribute("fuel", fuel);
     setTimeout(function(){Metres(that)}, 100);
 } else {
-    
+    that.parentNode.querySelector(".Game_rocket1").setAttribute("active", false)
+    document.body.querySelector("main .Game_Main .Game_Canvas_Borders .Game_Canvas").setAttribute("playing", false)
 };
 
-}
+};
 
 function RocketLaunched(that) {
 that.style.display = "none"
 
-
 that.parentNode.querySelector(".Game_Land_png").style.transition = that.parentNode.querySelector(".Game_rocket1").getAttribute("speed")
 that.parentNode.querySelector(".Game_Land_png").style.bottom = "-20%" 
 Metres(that)
-}
+};
+
+function keys(e) {
+    let key = e.key
+    if(document.body.querySelector("main .Game_Main .Game_Canvas_Borders .Game_Canvas").getAttribute("playing") == "true") {
+    if(key.toLowerCase() === "d") {
+    
+    }
+    };
+    };
+
+document.addEventListener("keydown", keys);
